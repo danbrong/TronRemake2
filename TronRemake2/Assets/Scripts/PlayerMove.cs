8c84da1b2580aa57c1f444a70f23292656a27059
danbrong <bchang91@gmail.com>
2023-10-02 15:47:34 -0700

    Movement Code Version 2

--- TronRemake2/Assets/Scripts/PlayerMove.cs
+++ TronRemake2/Assets/Scripts/PlayerMove.cs
@@ -12,9 +12,19 @@ public class PlayerMove : MonoBehaviour
     public float speed;
     public float xMoveInitial = 0;
     public float yMoveInitial = 1;
-    public float xMoveCurrent;
-    public float yMoveCurrent;
     Rigidbody rigb;
+
+    float upMove = 1;
+    float downMove = -1;
+    float leftMove = -1;
+    float rightMove = 1;
+    bool upReverse = false;
+    bool downReverse = true;
+    bool leftReverse = true;
+    bool rightReverse = true;
+
+    float directionXmove;
+    float directionYmove;
 
     private Vector2 direction;
     private Quaternion rotation = Quaternion.identity;
@@ -34,7 +44,44 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        getInput();
+        // EXPERIMENTAL: Trying to connect movement direction with reverse status
+        if (Input.GetKey(W))
+            directionYmove = 1;
+
+        if (directionYmove == 1)
+            upReverse = false;
+            downReverse = true;
+            leftReverse = true;
+            rightReverse = true;
+
+        if (Input.GetKey(S))
+            directionYmove = -1;
+
+        if (directionYmove == -1)
+            upReverse = true;
+            downReverse = false;
+            leftReverse = true;
+            rightReverse = true;
+
+        if (Input.GetKey(A))
+            directionXmove = -1;
+
+        if (directionXmove == -1)
+            upReverse = true;
+            downReverse = true;
+            leftReverse = false;
+            rightReverse = true;
+
+        if (Input.GetKey(D))
+            directionXmove = 1;
+
+        if (directionXmove == 1)
+            upReverse = true;
+            downReverse = true;
+            leftReverse = true;
+            rightReverse = false;
+
+        Rotate();
         Move();
     }
 
@@ -44,31 +91,75 @@ public class PlayerMove : MonoBehaviour
         //transform.position += (Vector3)(direction * speed * Time.deltaTime);
         transform.rotation = rotation;
 
+        // Movement Code Redux
+        if (Input.GetKeyDown(W) && upReverse == false)
+            rigb.velocity = new Vector3(0, upMove, 0) * (speed *2);
+        if (Input.GetKeyDown(W) && downReverse == false)
+            rigb.velocity = new Vector3(0, upMove, 0) * (speed / 2);
+        if (Input.GetKeyDown(W) && upReverse == true)
+            rigb.velocity = new Vector3(0, upMove, 0) * speed;
+        if (Input.GetKeyDown(W) && downReverse == true)
+            rigb.velocity = new Vector3(0, upMove, 0) * speed;
 
+
+        if (Input.GetKeyDown(S) && downReverse == false)
+            rigb.velocity = new Vector3(0, downMove, 0) * (speed * 2);
+        if (Input.GetKeyDown(S) && upReverse == false)
+            rigb.velocity = new Vector3(0, downMove, 0) * (speed / 2);
+        if (Input.GetKeyDown(S) && downReverse == true)
+            rigb.velocity = new Vector3(0, downMove, 0) * speed;
+        if (Input.GetKeyDown(S) && upReverse == true)
+            rigb.velocity = new Vector3(0, downMove, 0) * speed;
+
+        
+        if (Input.GetKeyDown(A) && leftReverse == false)
+            rigb.velocity = new Vector3(leftMove, 0, 0) * (speed * 2);
+        if (Input.GetKeyDown(A) && rightReverse == false)
+            rigb.velocity = new Vector3(leftMove, 0, 0) * (speed / 2);
+        if (Input.GetKeyDown(A) && leftReverse == true)
+            rigb.velocity = new Vector3(leftMove, 0, 0) * speed;
+        if (Input.GetKeyDown(A) && rightReverse == true)
+            rigb.velocity = new Vector3(leftMove, 0, 0) * speed;
+
+
+        if (Input.GetKeyDown(D) && rightReverse == false)
+            rigb.velocity = new Vector3(rightMove, 0, 0) * (speed * 2);
+        if (Input.GetKeyDown(D) && leftReverse == false)
+            rigb.velocity = new Vector3(rightMove, 0, 0) * (speed / 2);
+        if (Input.GetKeyDown(D) && rightReverse == true)
+            rigb.velocity = new Vector3(rightMove, 0, 0) * speed;
+        if (Input.GetKeyDown(D) && leftReverse == true)
+            rigb.velocity = new Vector3(rightMove, 0, 0) * speed;
+        
+
+        // ARCHIVE: Original Player Movement
         // Read Player Input and Get Corresponding Direction
-        float xMove = Input.GetAxisRaw("Horizontal");
-        float yMove = Input.GetAxisRaw("Vertical");
+
+        //float xMove = Input.GetAxisRaw("Horizontal");
+        //float yMove = Input.GetAxisRaw("Vertical");
 
         // Player Movement Change based on Input
-        if (xMove == -1)
-        {
-            rigb.velocity = new Vector3(xMove, 0, 0) * speed;
-        }
-        if (xMove == 1)
-        {
-            rigb.velocity = new Vector3(xMove, 0, 0) * speed;
-        }
-        if (yMove == -1)
-        {
-            rigb.velocity = new Vector3(0, yMove, 0) * speed;
-        }
-        if (yMove == 1)
-        {
-            rigb.velocity = new Vector3(0, yMove, 0) * speed;
-        }
+        //if (xMove == -1)
+        //{
+        //    rigb.velocity = new Vector3(xMove, 0, 0) * speed;
+        //}
+        //if (xMove == 1)
+        //{
+        //    rigb.velocity = new Vector3(xMove, 0, 0) * speed;
+        //}
+        //if (yMove == -1)
+        //{
+        //    rigb.velocity = new Vector3(0, yMove, 0) * speed;
+        //}
+        //if (yMove == 1)
+        //{
+        //    rigb.velocity = new Vector3(0, yMove, 0) * speed;
+        //}
     }
-    // Keyboard input by player
-    private void getInput()
+
+
+    // Player Rotation Control based on Input
+    private void Rotate()
     {
         direction = zero;
 
