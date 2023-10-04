32b014acdc6662783acc3ad9c9ee5f71517d7275
danbrong <bchang91@gmail.com>
2023-10-04 15:10:50 -0700

    Working Light Trail, No Collision

---
+++ TronRemake2/Assets/Scripts/TrailGeneration.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class TrailGeneration : MonoBehaviour
+{
+    // Variable Declaration
+    public GameObject TrailWall;
+    Collider wall;
+
+    // Wall Spawn Function
+    void SpawnWall()
+    {
+        GameObject g = Instantiate(TrailWall);
+        wall = g.GetComponent<Collider>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnWall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Copy of SpawnWall Declaration, for some reason helps smooth out movement
+        void SpawnWall()
+        {
+            GameObject g = Instantiate(TrailWall, transform.position, Quaternion.identity);
+            wall = g.GetComponent<Collider>();
+        }
+        SpawnWall();
+    }
+
+    
+}
